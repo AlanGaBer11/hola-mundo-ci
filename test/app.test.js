9fc332c6e5db909837a938373494f2bf7f3100a3
alanGarcia <220258@utags.edu.mx>
2025-10-24 20:34:07 -0600

    Eliminación de una prueba y actualización del pipeline

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,3 @@ const holaMundo = require("../src/app");
 test('Debe retornar "Hola Mundo"', () => {
   expect(holaMundo()).toBe("Hola Mundo");
 });
-
-test("No debe retornar otra cosa", () => {
-  expect(holaMundo()).not.toBe("Adiós");
-});
